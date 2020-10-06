1c67a1bf2a33cff17202f1c579a65071f170b86b
Volodymyr Kornieiev <Volydemar2032@gmail.com>
2020-10-06 14:49:20 +0300

    Исходники и базовая настройка Webpack

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+};
